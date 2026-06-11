0afc591da9ba7e95703d89c8bc62f20f374ec752
LifeCare Developer <developer@lifecare.com>
2026-06-11 08:17:52 -0700

    fix: change babel preset from expo to react-native

--- life-care-app/babel.config.js
+++ life-care-app/babel.config.js
@@ -1,10 +1,7 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel'
-    ],
-    plugins: ['react-native-reanimated/plugin'],
-  };
+module.exports = {
+  presets: [
+    ['module:@react-native/babel-preset', { jsxImportSource: 'nativewind' }],
+    'nativewind/babel'
+  ],
+  plugins: ['react-native-reanimated/plugin'],
 };
